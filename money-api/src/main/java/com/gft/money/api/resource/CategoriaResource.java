d9bd89ef8ca11602906c55304d303067d4e11a54
Gesiane Pajarinen <gesiane.pajarinen@yahoo.com>
2020-11-19 14:13:14 -0300

    3.12 Usando event pra adicionar header location

--- money-api/src/main/java/com/gft/money/api/resource/CategoriaResource.java
+++ money-api/src/main/java/com/gft/money/api/resource/CategoriaResource.java
@@ -1,12 +1,13 @@
 package com.gft.money.api.resource;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +15,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.gft.money.api.event.RecursoCriadoEvent;
 import com.gft.money.api.model.Categoria;
 import com.gft.money.api.repository.CategoriaRepository;
 
@@ -27,6 +28,8 @@ public class CategoriaResource {
 	@Autowired
 	private CategoriaRepository cr;
 	
+	@Autowired
+	private ApplicationEventPublisher pub;
 	
 	@GetMapping
 	public List<Categoria> listarCategorias(){
@@ -38,10 +41,8 @@ public class CategoriaResource {
 	public ResponseEntity<Categoria> criarCategoria(@Valid @RequestBody Categoria categoria, HttpServletResponse response) {
 		Categoria categoriaSalva = cr.save(categoria);
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(categoriaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-		
-		return ResponseEntity.created(uri).body(categoriaSalva);
+		pub.publishEvent(new RecursoCriadoEvent(this, response, categoriaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaSalva);
 		
 	}
 	
--- money-api/src/main/java/com/gft/money/api/resource/PessoaResource.java
+++ money-api/src/main/java/com/gft/money/api/resource/PessoaResource.java
@@ -1,11 +1,11 @@
 package com.gft.money.api.resource;
 
-import java.net.URI;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.gft.money.api.event.RecursoCriadoEvent;
 import com.gft.money.api.model.Pessoa;
 import com.gft.money.api.repository.PessoaRepository;
 
@@ -26,15 +26,16 @@ public class PessoaResource {
 	@Autowired
 	private PessoaRepository pr;
 	
+	@Autowired
+	private ApplicationEventPublisher pub;
+	
 		
 	@PostMapping
 	public ResponseEntity<Pessoa> cadastrarPessoa(@Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
 		Pessoa pessoaSalva = pr.save(pessoa);
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}").buildAndExpand(pessoaSalva.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
-		
-		return ResponseEntity.created(uri).body(pessoaSalva);
+		pub.publishEvent(new RecursoCriadoEvent(this, response, pessoaSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 		
 	}
 	
