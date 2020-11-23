15f3d786cb67ff52283729878a4a342a0ad7ad3c
Gesiane Pajarinen <gesiane.pajarinen@yahoo.com>
2020-11-23 10:01:46 -0300

    "5.3 Desafio cadastrando lancamento OK"

--- money-api/src/main/java/com/gft/money/api/resource/LancamentoResource.java
+++ money-api/src/main/java/com/gft/money/api/resource/LancamentoResource.java
@@ -2,13 +2,21 @@ package com.gft.money.api.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.gft.money.api.event.RecursoCriadoEvent;
 import com.gft.money.api.model.Lancamento;
 import com.gft.money.api.repository.LancamentoRepository;
 
@@ -20,6 +28,9 @@ public class LancamentoResource {
 	@Autowired
 	private LancamentoRepository lr;
 	
+	@Autowired
+	private ApplicationEventPublisher pub;
+	
 	@GetMapping
 	public List<Lancamento> listar() {
 		return lr.findAll();
@@ -30,6 +41,15 @@ public class LancamentoResource {
 		Lancamento lancamento = lr.findById(codigo).orElse(null);
 
 		return lancamento != null ? ResponseEntity.ok(lancamento) : ResponseEntity.notFound().build();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Lancamento> cadastrarLancamento(@Valid @RequestBody Lancamento lancamento, HttpServletResponse response) {
+		Lancamento lancamentoSalva = lr.save(lancamento);
+		
+		pub.publishEvent(new RecursoCriadoEvent(this, response, lancamentoSalva.getCodigo()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalva);
+		
 	}
 	
 }
