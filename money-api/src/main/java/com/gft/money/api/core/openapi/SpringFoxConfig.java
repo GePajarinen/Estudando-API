440d9403fc83c613a6d49115e10700357b0671fc
Gesiane Pajarinen <gesiane.pajarinen@yahoo.com>
2020-11-25 08:49:41 -0300

    "aula 6. Editando o INFO da Documentação"

--- money-api/src/main/java/com/gft/money/api/core/openapi/SpringFoxConfig.java
+++ money-api/src/main/java/com/gft/money/api/core/openapi/SpringFoxConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -22,9 +24,21 @@ public class SpringFoxConfig implements WebMvcConfigurer{
 					.apis(RequestHandlerSelectors.basePackage("com.gft.money.api"))
 	//				.paths(PathSelectors.any())   //Igual ao de cima. Referência de outra opção.
 	//				.paths(PathSelectors.ant("/pessoas/*")) //Outra opção pra selecionar o que dispor na Documentação (bem legal)
-					.build();
+					.build()
+				.apiInfo(apiInfo());
 	}
 
+	
+	public ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Money Controlador API")
+				.description("Estudando API, OpenApi e Documentação")
+				.version("1.0")
+				.contact(new Contact("Gesiane","https://github.com/GePajarinen","gesaine.pajarinen@gft.com"))
+				.build();
+	}
+	
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("swagger-ui.html")
